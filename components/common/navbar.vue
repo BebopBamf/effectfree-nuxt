a19c60029ecc0dcd67cc565275f35f0cca2e994e
Euan Mendoza <bebopbamf@effectfree.dev>
2021-09-16 19:17:44 +1000

    nav component

---
+++ components/common/navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <c-box>
+        <c-flex p="2">
+            <c-box>
+                <c-text font-size="lg" font-weight="bold">EFFECT FREE</c-text>
+            </c-box>
+        </c-flex>
+    </c-box>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { CFlex, CText, CBox } from '@chakra-ui/vue';
+
+export default Vue.extend({
+    name: 'Navbar',
+    components: {
+        CFlex,
+        CBox,
+        CText,
+    },
+});
+</script>
